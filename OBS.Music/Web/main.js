40a4c6d6f826ad50586a004a80866b07ad3e0e3d
Marcus Aurelius <Marcus.Aurelius@gallimathias.de>
2017-10-29 18:42:07 +0100

    Add WebFiles

---
+++ OBS.Music/Web/main.js
@@ -0,0 +1,11 @@
+﻿function getValues() {
+    $.get("/Name", function (data) {
+        $(".Name").html(data + " - ");
+    });
+    $.get("/Source", function (data) {
+        $(".Source").html(data);
+    });
+    $.get("/Licence", function (data) {
+        $(".Licence").html(data);
+    });
+}
